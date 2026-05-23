a334825c3bea3d01052e3f5c295cf97f9a003b4c
Maximilian Eberlein <meberlein@ethz.ch>
2026-05-23 19:29:44 +0200

    cleanup

--- src/orca_teleop/ingress/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Ingress layer for ORCA teleoperation."""
-
-from orca_teleop.ingress.server import DEFAULT_PORT, HandLandmarks, IngressServer
-
-__all__ = [
-    "DEFAULT_PORT",
-    "HandLandmarks",
-    "IngressServer",
-]
